2e91c53f45ac414a1e203e6bb54cffaf4aa1cb0b
AndreiPetoukhov1986 <oziglena@mail.ru>
2022-12-20 21:11:56 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
         var dog = 8.0;
-        System.out.println(dog);
-        dog = dog + 4;
-        System.out.println(dog);
-        dog = dog - 3.5;
-        System.out.println(dog);
-
+        System.out.println("dog="+dog);
         var cat = 3.6;
-        System.out.println(cat);
-        cat = cat + 4;
-        System.out.println(cat);
-        cat = cat - 1.6;
-        System.out.println(cat);
-
+        System.out.println("cat="+cat);
         var paper = 763789;
-        System.out.println(paper);
+        System.out.println("paper="+paper);
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        System.out.println("dog="+dog);
+        cat = cat + 4;
+        System.out.println("cat="+cat);
         paper = paper + 4;
-        System.out.println(paper);
+        System.out.println("paper="+paper);
+        System.out.println("Задача 3");
+        dog = dog - 3.5;
+        System.out.println("dog="+dog);
+        cat = cat - 1.6;
+        System.out.println("cat="+cat);
         paper = paper - 7639;
-        System.out.println(paper);
+        System.out.println("paper="+paper);
     }
 }
